01bfe51d2ba0f2a8e6e312a15df44ab3cdeb8973
Aleksandr Yemelianov <emelyanon@gmail.com>
2023-09-13 21:21:01 +0200

    all module-2

--- src/components/AvatarAuth/AvatarAuth.js
+++ src/components/AvatarAuth/AvatarAuth.js
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
 import React from 'react'
 import { styles } from './AvatarAuthStyle'
 
 const AvatarAuth = () => {
-  return (
-    <View style={styles.wrapper}>
-      <Text>AvatarAuth</Text>
-    </View>
-  )
+    return (
+        <View style={styles.wrapper}>
+            <TouchableOpacity style={styles.wrapperSvg}>
+                <AntDesign name="pluscircleo" size={25} style={styles.svgAdd} />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 export default AvatarAuth
--- src/components/AvatarAuth/AvatarAuthStyle.js
+++ src/components/AvatarAuth/AvatarAuthStyle.js
@@ -1,11 +1,10 @@
 import { StyleSheet } from "react-native";
-import { backgroundTextColor } from "../../assets/colors/colors";
+import { accentColor, backgroundColor, backgroundTextColor } from "../../assets/colors/colors";
 
 export const styles = StyleSheet.create({
     wrapper: {
         position: 'absolute',
-        // top: 0,
-        // left: '50%',
+        top: -60,
         width: 120,
         height: 120,
         borderRadius: 16,
@@ -13,5 +12,16 @@ export const styles = StyleSheet.create({
         // justifyContent: 'flex-end',
         // alignItems: 'center',
         backgroundColor: backgroundTextColor,
+    },
+    wrapperSvg: {
+        position: 'absolute',
+        backgroundColor: backgroundColor,
+        right: -12,
+        bottom: 12,
+        width: 25,
+        borderRadius: '50%'
+    },
+    svgAdd: {
+        color: accentColor,
     },
 });
